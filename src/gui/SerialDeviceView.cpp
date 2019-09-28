5963e9dfc12948f3aa5e576239a1768b12a909ef
Naoto Kondo <cgigcp3yqt@gmail.com>
2019-09-28 23:51:14 +0900

    Don't escape serial device info

--- src/gui/SerialDeviceView.cpp
+++ src/gui/SerialDeviceView.cpp
@@ -75,19 +75,21 @@ void SerialDeviceView::updateDeviceInfo(const QString &name) {
 
       QString text;
 
-      text += "Port Name:\t\t" + port.portName() + "\n";
-      text += "Description:\t\t" + port.description() + "\n";
-      text += "Manufacturer:\t\t" + port.manufacturer() + "\n";
-      text += "Serial Number:\t\t" + port.serialNumber() + "\n";
-      text += "System Location:\t" + port.systemLocation() + "\n";
+      text += "Port Name: " + port.portName() + "\n";
+      text += "Description: " + port.description() + "\n";
+      text += "Manufacturer: " + port.manufacturer() + "\n";
+      text += "Serial Number: " + port.serialNumber() + "\n";
+      text += "System Location: " + port.systemLocation() + "\n";
 
       if (port.hasVendorIdentifier())
-        text += "Vendor Identifier:\t" +
-                QString::number(port.vendorIdentifier()) + "\n";
+        text +=
+            "Vendor Identifier: " + QString::number(port.vendorIdentifier()) +
+            "\n";
 
       if (port.hasProductIdentifier())
-        text += "Product Identifier:\t" +
-                QString::number(port.productIdentifier()) + "\n";
+        text +=
+            "Product Identifier: " + QString::number(port.productIdentifier()) +
+            "\n";
 
       ui->textBrowserInfo->setText(text);
 
