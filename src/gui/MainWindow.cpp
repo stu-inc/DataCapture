2371a79d506118626dd310fe3367de4b7ee13033
Naoto Kondo <cgigcp3yqt@gmail.com>
2019-09-29 21:58:26 +0900

    Set window title automatically

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -1,9 +1,12 @@
 #include "MainWindow.hpp"
+#include "autodef.hpp"
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+  setWindowTitle(QString(APPLICATION_NAME) + " v" +
+                 QString(APPLICATION_VERSION));
 }
 
 MainWindow::~MainWindow() { delete ui; }
